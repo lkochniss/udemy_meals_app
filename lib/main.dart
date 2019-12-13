67eba59db4f05a46cc6436170c7d2059e29f42d6
LukasKochniss <lukas.kochniss@the-fittest.de>
2019-12-13 16:46:13 +0100

    end of lesson 158

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,34 +9,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(fontFamily: 'RobotoCondesed', fontSize: 24),
+        ),
       ),
-      home: MyHomePage(title: 'DeliMeals'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
